9dc4f939fd7f136d57b07eace7c7f6c0974c1c88
Thomas MG Legalche <thomas.legalche@gmail.com>
2022-12-15 19:37:42 +0100

    creation frontend (position not done) of myeventScreen

--- screens/MyEventScreen/MyEventScreen.js
+++ screens/MyEventScreen/MyEventScreen.js
@@ -1,7 +1,6 @@
 import {
-    Image,
     KeyboardAvoidingView,
-    Platform,
+    Keyboard,
     StyleSheet,
     Text,
     View,
@@ -11,21 +10,34 @@ import {
     ImageBackground,
   } from "react-native";
   import { SelectList } from "react-native-dropdown-select-list";
+  import DatePicker from "@react-native-community/datetimepicker";
   import { useState } from "react";
 
   export default function EventScreen({navigation}) {
 
-    const data2 = ["Amateur", "Medium", "Semi-Pro", "Pro"];
-    const [sport, setSport] = useState('')
+    const data2 = ["Running", "Hiking", "Snorkeling", "Tennis", "Cycling", "Basketball", "Football"];
+    const [sport, setSport] = useState('');
+    const [dateEvent, setDateEvent] = useState(new Date());
+    const [hourEvent, setHourEvent] = useState(new Date());
+    const [searchAddress, setSearchAddress] = useState('');
+    const [eventDescription, setEventDescription] = useState('')
+
+    const dateEventSelected = (event, value) => {
+      setDateEvent(value)
+    }
+
+    const hourEventSelected = (event, value) => {
+      setHourEvent(value)
+    }
 
     return (
         <ImageBackground style={styles.imgBackground} source={require('../../assets/background.jpg')}>
-        <View style={styles.container}>
+        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
             
             <View style={styles.headerContainer}>
             <Text style={styles.headerText}>My Event</Text>
             </View>
-
+{/* WHICH SPORT ? */}
             <View style={styles.sectionContainer}>
                 <Text style={styles.questionText}>Which sport ?</Text>
                     <SelectList
@@ -41,32 +53,75 @@ import {
                     />
             </View>
 
+
+{/* DATE SELECTION */}
             <View style={styles.sectionContainer}>
-                <Text style={styles.textsmall}>Date ? -- datepicker</Text>
+                <Text style={styles.questionText}>Select a date</Text>
+                <DatePicker
+                style={styles.calendar}
+                mode="date"
+                value={dateEvent}
+                minimumDate={new Date()}
+                textColor="#E74C3C"
+                accentColor="#E74C3C"
+                onChange={dateEventSelected}
+                format="hh:mm"/>
             </View>
 
+{/* HOUR SELECTION */}
             <View style={styles.sectionContainer}>
-                <Text style={styles.textsmall}>Hour ? -- datetimepicker</Text>
+                <Text style={styles.questionText}>Select an hour</Text>
+                <DatePicker
+                style={styles.hour}
+                mode="time"
+                value={hourEvent}
+                onChange={hourEventSelected}
+                textColor="#E74C3C"
+                accentColor="#E74C3C"
+                minuteInterval={10}/>
             </View>
 
-            <View style={styles.sectionContainer}>
-                <Text style={styles.textsmall}>Where ? -- search address</Text>
-            </View>
-
+{/* ADDRESS SELECTION */}
+            
             <View style={styles.descriptionContainer}>
-                <Text style={styles.text}>Description of the event</Text>
+                <Text style={styles.questionText}>Where ?</Text>
+                <TextInput
+                onSubmit={Keyboard.dismiss}
+                style={styles.addressInput}
+                placeholder="Enter an address"
+                placeholderTextColor="#ccd1e8"
+                onChangeText={(value) => {setSearchAddress(value);}}/>
             </View>
 
+{/* EVENT DESCRIPTION */}
+            <View style={styles.descriptionContainer}>
+                <Text style={styles.questionText}>Enter a short description of your event</Text>
+                <TextInput
+                onSubmit={Keyboard.dismiss}
+                style={styles.descriptionInput}
+                placeholder="Enter a short description"
+                placeholderTextColor="#ccd1e8"
+                onChangeText={(value) => {setEventDescription(value);}}/>
+            </View>
+
+{/* BUTTON CANCEL AND CONFIRM */}
             <View style={styles.buttonContainer}>
-                <Text style={styles.text}>cancel</Text>
-                <Text style={styles.text}>confirm</Text>
+              <TouchableOpacity
+              style={styles.button}>
+                <Text style={styles.textButton}>CANCEL</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+              style={styles.button}>
+                <Text style={styles.textButton}>CONFIRM</Text>
+              </TouchableOpacity>
             </View>
 
 
 
 
 
-        </View>
+        </KeyboardAvoidingView>
         </ImageBackground>
     );
   }
@@ -82,42 +137,82 @@ import {
         justifyContent: "flex-start",
         marginTop: 45,
       },
-      // HEADER
+      // CONTAINERS
       headerContainer: {
         width: '100%',
         height: '10%',
         paddingLeft: 10,
       },
-      headerText: {
-        fontSize: 45,
-        fontWeight: "700",
-        color: "white",
-        fontFamily: "Poppins-Bold",
-        textShadowColor: "black",
-        textShadowOffset: { width: -1, height: 1 },
-        textShadowRadius: 20,
-      },
     sectionContainer: {
         width: '100%',
-        height: '10%',
         borderWidth: 2,
-        backgroundColor: 'lightgrey',
         marginBottom: 4,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginVertical: 10,
+        paddingHorizontal: 20,
     },
-    // FIRST QUESTION
       descriptionContainer: {
         width: '100%',
         height: '23%',
-        backgroundColor: 'skyblue',
+        flexDirection: 'column',
+        borderWidth: 2,
+        paddingHorizontal: 20,
       },
       buttonContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'space-around',
         width: '100%',
         height: '17%',
-        backgroundColor: 'lightcoral',
+        borderWidth: 2,
+      },
+      calendar: {
+        backgroundColor: "white",
+        opacity: 0.9,
+      },
+      hour: {
+        backgroundColor: "white",
+        opacity: 0.9,
+      },
+      addressInput: {
+        width: "90%",
+        height: '35%',
+        borderColor: "#E74C3C",
+        borderWidth: 2,
+        borderRadius: 10,
+        fontSize: 16,
+        backgroundColor: 'white',
+        paddingLeft: 10,
+      },
+      descriptionInput: {
+        width: "90%",
+        height: "35%",
+        borderColor: "#E74C3C",
+        borderWidth: 2,
+        borderRadius: 10,
+        fontSize: 16,
+        backgroundColor: 'white',
+        paddingLeft: 10,
+      },
+      button: {
+        alignItems: "center",
+        justifyContent: "center",
+        width: "30%",
+        height: "40%",
+        backgroundColor: "#E74C3C",
+        borderRadius: 10,
       },
     //   TEXT
+    headerText: {
+      fontSize: 45,
+      fontWeight: "700",
+      color: "white",
+      fontFamily: "Poppins-Bold",
+      textShadowColor: "black",
+      textShadowOffset: { width: -1, height: 1 },
+      textShadowRadius: 20,
+    },
     questionText: {
       fontFamily: "Poppins-Medium",
       fontSize: 20,
@@ -125,10 +220,10 @@ import {
       backgroundColor: "white",
       marginTop: 15,
     },
-    text: {
-        fontSize: 40
-    },
-    textsmall: {
-        fontSize: 20
+    textButton: {
+      color: "#ffffff",
+      fontWeight: "600",
+      fontSize: 16,
+      fontFamily: "Poppins-Bold",
     },
   })
