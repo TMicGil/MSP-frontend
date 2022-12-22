2902ed493e31e9a9903725f644980c3b2e192814
andres malaisse <malaisse_andres@sfr.fr>
2022-12-22 11:55:20 +0100

    logout and delete account

--- reducers/user.js
+++ reducers/user.js
@@ -1,11 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: { firstname: null, email: null, token: null, location: {latitude: null, longitude: null, latitudeDelta: null, longitudeDelta: null} },
+  value: {
+    firstname: null,
+    email: null,
+    token: null,
+    location: {
+      latitude: null,
+      longitude: null,
+      latitudeDelta: null,
+      longitudeDelta: null,
+    },
+  },
 };
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
   reducers: {
     signIn: (state, action) => {
@@ -14,25 +24,25 @@ export const userSlice = createSlice({
       state.value.token = action.payload.token;
     },
     signUp: (state, action) => {
-        state.value.firstname = action.payload.firstname;
-        state.value.email = action.payload.email;
-        state.value.token = action.payload.token;
+      state.value.firstname = action.payload.firstname;
+      state.value.email = action.payload.email;
+      state.value.token = action.payload.token;
     },
     userGeoLocation: (state, action) => {
       state.value.location.latitude = action.payload.latitude;
       state.value.location.longitude = action.payload.longitude;
       state.value.location.latitudeDelta = action.payload.latitudeDelta;
       state.value.location.longitudeDelta = action.payload.longitudeDelta;
-  },
+    },
     logout: (state) => {
-        state.value.firstname = null;
-        state.value.email = null;
-        state.value.password = null;
-        state.value.token = null;
-        state.value.userId = null;
+      state.value.firstname = null;
+      state.value.email = null;
+      state.value.password = null;
+      state.value.token = null;
+      state.value.userId = null;
     },
   },
 });
 
-export const { signIn, signUp, userGeoLocation } = userSlice.actions;
+export const { signIn, signUp, userGeoLocation, logout } = userSlice.actions;
 export default userSlice.reducer;
--- screens/SettingScreen/SettingsScreen.js
+++ screens/SettingScreen/SettingsScreen.js
@@ -1,40 +1,115 @@
 import {
-    Image,
-    KeyboardAvoidingView,
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-    Modal,
-    TextInput,
-    TouchableOpacity,
-    ImageBackground,
-  } from "react-native";
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Alert,
+} from "react-native";
+import { logout } from "../../reducers/user";
+import { useDispatch, useSelector } from "react-redux";
 
-  export default function SettingScreen({navigation}) {
-    return (
-        <ImageBackground style={styles.imgBackground} source={require('../../assets/background.jpg')}>
-        <View style={styles.container}>
+export default function SettingScreen({ navigation }) {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
 
-                <Text style={styles.text}>SETTINGS</Text>
+  const handleLogout = () => {
+    dispatch(logout());
+    navigation.navigate("Login");
+  };
 
-        </View>
-        </ImageBackground>
-    );
-  }
+  const handleDeleteAccount = () => {
+    const body = {
+      token: user.token,
+    };
+    fetch("https://msp-backend.vercel.app/users/delete", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          Alert.alert("Confirmation :", "Your account has been deleted", {
+            cancelable: true,
+          });
+          navigation.navigate("Login");
+        }
+      });
+  };
 
-  const styles = StyleSheet.create({
-    imgBackground: {
-        height: '100%',
-        width: '100%',
-      },
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-    //   TEXT
-    text: {
-        fontSize: 40
-    },
-  })
+  return (
+    <ImageBackground
+      style={styles.imgBackground}
+      source={require("../../assets/background.jpg")}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.logoutBtn}
+          onPress={() => handleLogout()}
+        >
+          <Text style={styles.logoutText}>LOGOUT</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.deleteBtn}
+          onPress={() => handleDeleteAccount()}
+        >
+          <Text style={styles.deleteText}>DELETE ACCOUNT</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  imgBackground: {
+    height: "100%",
+    width: "100%",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: "10%",
+  },
+  //   TEXT
+  text: {
+    fontSize: 40,
+  },
+  logoutBtn: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    height: "10%",
+    backgroundColor: "#E74C3C",
+    borderRadius: 20,
+    marginTop: "150%",
+  },
+  logoutText: {
+    color: "#ffffff",
+    fontWeight: "600",
+    fontSize: 25,
+    fontFamily: "Poppins-Light",
+  },
+  deleteBtn: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    height: "10%",
+    backgroundColor: "#E74C3C",
+    borderRadius: 20,
+    marginTop: 30,
+  },
+  deleteText: {
+    color: "#ffffff",
+    fontWeight: "600",
+    fontSize: 25,
+    fontFamily: "Poppins-Light",
+  },
+});
